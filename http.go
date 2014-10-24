ffd59894dc3bccc34458781cc9b4e84e67350f37
Stephan Zeissler <stephan@dockzero.io>
2014-10-24 12:48:11 +0200

    HttpStream

--- http.go
+++ http.go
@@ -7,6 +7,24 @@ import (
 	"net/http"
 )
 
+func HttpStream(res http.ResponseWriter, w io.Writer, r io.ReadCloser) error {
+	// type assert to http.Flusher to be able to stream journald's output
+	f, ok := res.(http.Flusher)
+	if !ok {
+		return errgo.Newf("response writer is not a flusher")
+	}
+	wf := &iostream.WriteFlusher{W: w, Flusher: f}
+
+	// type assert to http.CloseNotifier to be able to handle disconnection of clients
+	cn, ok := res.(http.CloseNotifier)
+	if !ok {
+		return errgo.Newf("response writer is not a close notifier")
+	}
+	closeChan := cn.CloseNotify()
+
+	return iostream.Stream(wf, r, closeChan)
+}
+
 // WriteFlusher is a port of Docker's implementation used in their API.
 // (https://github.com/docker/docker/blob/9ae3134dc9f0652ef48ec1fd445f42d8fe26de35/utils/utils.go#L269)
 // It combines io.Writer and http.Flusher to enable streaming of constant data
