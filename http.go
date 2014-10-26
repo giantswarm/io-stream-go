123e1077413485b847641cb1875f61237273eea4
Stephan Zeissler <stephan@dockzero.io>
2014-10-26 11:30:38 +0100

    Reverted port -> copy

--- http.go
+++ http.go
@@ -37,7 +37,7 @@ func HttpStream(res http.ResponseWriter, w io.Writer, r io.ReadCloser) error {
 	return Stream(wf, r, closeChan)
 }
 
-// WriteFlusher is a copy of Docker's implementation used in their API.
+// WriteFlusher is a port of Docker's implementation used in their API (we dropped the Mutex).
 // (https://github.com/docker/docker/blob/9ae3134dc9f0652ef48ec1fd445f42d8fe26de35/utils/utils.go#L269)
 // It combines io.Writer and http.Flusher to enable streaming of constant data
 // flows via http connections.
